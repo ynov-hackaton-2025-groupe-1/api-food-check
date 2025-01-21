0037854ad1f2b8d81f8e7a07d801ae45ca038b5e
ThomasGrare <thomasgrare64@gmail.com>
2025-01-21 09:13:55 +0100

    fix(login): route controller

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,8 +1,8 @@
 import {login, register} from "../controllers/auth_controller.js";
 
 const authRoutes = async (fastify, options) => {
-  fastify.post("/register", login);
-  fastify.post("/login", register);
+  fastify.post("/register", register);
+  fastify.post("/login", login);
 }
 
 export default authRoutes;
